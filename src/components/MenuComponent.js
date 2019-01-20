2d039c4591f791708d1f4aef23e1247df7dabdbd
JK <krynski.jacek@gmail.com>
2019-01-20 10:44:27 +0000

    Container Component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -18,14 +18,12 @@ class Menu extends Component {
         this.setState({ selectedDish: dish});
     }
 
-
-
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
               <div  className="col-12 col-md-5 m-1">
                 <Card key={dish.id}
-                  onClick={() => this.onDishSelect(dish)}>
+                  onClick={() => this.props.onClick(dish.id)}>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
